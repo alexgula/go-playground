2c4efbd7afbf5084a78a69152d472f95dd562610
Oleksandr Gula <alexgula@gmail.com>
2015-02-09 01:40:56 +0100

    Fixed multiple sets by implementing actual counting of bits.

--- facet/facet.go
+++ facet/facet.go
@@ -1,17 +1,24 @@
 package facet
 
 type Facet struct {
-	bitCount int
+	bits byte
 }
 
 func New() *Facet {
 	return &Facet{}
 }
 
-func (facet *Facet) Set(bit int) {
-	facet.bitCount++
+func (facet *Facet) Set(bit uint64) {
+	facet.bits = facet.bits | (1 << bit)
 }
 
-func (facet *Facet) Count() int {
-	return facet.bitCount
+func (facet *Facet) Count() (count uint64) {
+	bits := facet.bits
+	for i := 0; i < 8; i++ {
+		if bits&1 == 1 {
+			count++
+		}
+		bits >>= 1
+	}
+	return
 }
