04103c74fac578fab2d467e28a22ce24d33022f1
Oleksandr Gula <alexgula@gmail.com>
2015-02-09 01:50:39 +0100

    Refactored last test, added test for setting bigger bits.

--- facet/facet_test.go
+++ facet/facet_test.go
@@ -30,7 +30,19 @@ func TestCanSetSameBitAndCountBits(t *testing.T) {
 	facet := New()
 	facet.Set(0)
 	facet.Set(0)
-	if cnt := facet.Count(); cnt != 1 {
-		t.Fatalf("Expected facet to hold 1 bit, got %v instead", cnt)
+	var expected uint64 = 1
+	if cnt := facet.Count(); cnt != expected {
+		t.Fatalf("Expected facet to hold %v bit, got %v instead", expected, cnt)
+	}
+}
+
+func TestCanSetBiggerBitsAndCountBits(t *testing.T) {
+	facet := New()
+	facet.Set(0)
+	facet.Set(0)
+	facet.Set(8)
+	var expected uint64 = 2
+	if cnt := facet.Count(); cnt != expected {
+		t.Fatalf("Expected facet to hold %v bits, got %v instead", expected, cnt)
 	}
 }
