3702b239e322a93a48b9728845e8b15ae37ac6a8
Oleksandr Gula <alexgula@gmail.com>
2015-02-09 01:26:25 +0100

    Added test for count of set bits.

--- facet/facet_test.go
+++ facet/facet_test.go
@@ -17,3 +17,11 @@ func TestCanCountBits(t *testing.T) {
 	facet := New()
 	facet.Count()
 }
+
+func TestCanSetAndCountBits(t *testing.T) {
+	facet := New()
+	facet.Set(0)
+	if cnt := facet.Count(); cnt != 1 {
+		t.Fatalf("Expected facet to hold 1 bit, got %v instead", cnt)
+	}
+}
