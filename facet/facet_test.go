54aa337f739b1b87912cda86126b304113b745f1
Oleksandr Gula <alexgula@gmail.com>
2015-03-10 10:06:49 +0100

    Added more tests.

--- facet/facet_test.go
+++ facet/facet_test.go
@@ -46,9 +46,29 @@ func TestCanSetAndClearAndCountBits(t *testing.T) {
 func TestCanSetBiggerBitsAndCountBits(t *testing.T) {
 	f := New()
 	f.Set(0)
-	f.Set(0)
 	f.Set(8)
 	f.expectCount(t, 2)
+}
+
+func TestCanSetAndClearBiggerBitsAndCountBits(t *testing.T) {
+	f := New()
+	f.Set(8)
+	f.Clear(8)
+	f.expectCount(t, 0)
+}
+
+func TestCanSetAndDoubleClearBiggerBitsAndCountBits(t *testing.T) {
+	f := New()
+	f.Set(8)
+	f.Clear(8)
+	f.Clear(8)
+	f.expectCount(t, 0)
+}
+
+func TestCanClearHugeBitsAndCountBits(t *testing.T) {
+	f := New()
+	f.Clear(1000000000000000)
+	f.expectCount(t, 0)
 }
 
 func TestCanSetBigBitsAndCountBits(t *testing.T) {
@@ -57,23 +77,55 @@ func TestCanSetBigBitsAndCountBits(t *testing.T) {
 	f.expectCount(t, 1)
 }
 
+func TestCanSetAndClearBigBitsAndCountBits(t *testing.T) {
+	f := New()
+	f.Set(10000000)
+	f.Set(10000001)
+	f.Clear(10000001)
+	f.expectCount(t, 1)
+}
+
 func TestCanSetAllBiggerBitsAndCountBits(t *testing.T) {
 	f := New()
-	var expected uint = 32
-	f.setAllBits(expected)
-	f.expectCount(t, expected)
+	var set uint = 32
+	f.setAllBits(set)
+	f.expectCount(t, set)
+}
+
+func TestCanSetAllBiggerBitsAndClearSomeBitsAndCountBits(t *testing.T) {
+	f := New()
+	var set uint = 32
+	var clearStart uint = 8
+	var clearEnd uint = 16
+	f.setAllBits(set)
+	f.clearAllBits(clearStart, clearEnd)
+	f.expectCount(t, set-(clearEnd-clearStart))
 }
 
 func TestCanSetAllBigBitsAndCountBits(t *testing.T) {
 	f := New()
-	var expected uint = 10000000
-	f.setAllBits(expected)
-	f.expectCount(t, expected)
+	var set uint = 10000000
+	f.setAllBits(set)
+	f.expectCount(t, set)
+}
+
+func TestCanSetAllBigBitsAndclearbigBitAndCountBits(t *testing.T) {
+	f := New()
+	var set uint = 10000000
+	f.setAllBits(set)
+	f.Clear(set - 1)
+	f.expectCount(t, set-1)
 }
 
 func (f *Facet) setAllBits(count uint) {
 	for i := uint(0); i < count; i++ {
 		f.Set(i)
+	}
+}
+
+func (f *Facet) clearAllBits(start uint, end uint) {
+	for i := start; i < end; i++ {
+		f.Clear(i)
 	}
 }
 
