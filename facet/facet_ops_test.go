1a57b07636e8081ad7dbd92738512bbf1300e449
Oleksandr Gula <alexgula@gmail.com>
2015-03-11 17:34:47 +0100

    Added failing tests for OR operation.

--- facet/facet_ops_test.go
+++ facet/facet_ops_test.go
@@ -10,10 +10,24 @@ func TestCanCalcAndOnEmpty(t *testing.T) {
 	f.expectCount(t, 0)
 }
 
+func TestCanCalcOrOnEmpty(t *testing.T) {
+	f, o := New(), New()
+	f.Or(o)
+	f.expectCount(t, 0)
+}
+
 func TestSetAndCanCalcAnd(t *testing.T) {
 	f, o := New(), New()
 	f.Set(20)
 	o.Set(20)
 	f.And(o)
 	f.expectCount(t, 1)
+}
+
+func TestSetAndCanCalcOr(t *testing.T) {
+	f, o := New(), New()
+	f.Set(20)
+	o.Set(40)
+	f.Or(o)
+	f.expectCount(t, 2)
 }
