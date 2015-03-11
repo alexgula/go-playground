5b8e74b097d0065cf52f4699f3db31ffcef26576
Oleksandr Gula <alexgula@gmail.com>
2015-03-11 17:14:38 +0100

    Added more tests.

--- facet/facet_ops_test.go
+++ facet/facet_ops_test.go
@@ -4,7 +4,13 @@ import (
 	"testing"
 )
 
-func TestCanCalcAnd(t *testing.T) {
+func TestCanCalcAndOnEmpty(t *testing.T) {
+	f, o := New(), New()
+	f.And(o)
+	f.expectCount(t, 0)
+}
+
+func TestSetAndCanCalcAnd(t *testing.T) {
 	f, o := New(), New()
 	f.Set(20)
 	o.Set(20)
