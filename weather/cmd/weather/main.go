9683f7fa42c90debc554fb3963b86de786edbcf4
Oleksandr Gula <alexgula@gmail.com>
2016-04-20 21:54:02 +0200

    Add command line weather client

---
+++ weather/cmd/weather/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alexgula/go-playground/weather"
+)
+
+func getWeather(key string, city string) (weather.Data, error) {
+	data, err := weather.NewApi(key).Url().ByName(city).Query()
+	if err != nil {
+		return weather.Data{}, err
+	}
+
+	data.City = city
+	data.Main.Celsius = data.Main.Kelvin - 273.15
+
+	return data, nil
+}
+
+func main() {
+	data, err := getWeather("602e5a1c5cb62e61550a72adf8726063", "Gdansk")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println(data)
+}
