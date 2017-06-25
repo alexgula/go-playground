0e2181af560fcda1b1d8b4ef59d3b0b246b233ae
Oleksandr Gula <alexgula@gmail.com>
2017-06-25 16:06:31 +0200

    Change to batch processing

--- mmap/main.go
+++ mmap/main.go
@@ -33,11 +33,11 @@ func (w *watch) Restart() {
 }
 
 func main() {
-	var valueCount = 1 * 1024 * 1024 * 1024
-	var valueSize = 1
+	var valueCount = 1 * 1024 * 1024
+	var valueSize = 1024
 	var capacity = valueCount * valueSize
-	var transformCount = 1 * 1024 * 1024 * 1024
-	var trackFrequency = 16 * 1024 * 1024
+	var transformCount = 1 * 1024 * 1024
+	var trackFrequency = 128 * 1024
 
 	var w = newWatch()
 
@@ -57,35 +57,46 @@ func main() {
 	w.Restart()
 	fmt.Printf("Map created in %v\n", w.elapsed)
 
-	for i := 0; i < capacity; i++ {
-		m[i] = 0
+	var zeroes [1024]byte
+	for i := 0; i < valueCount; i++ {
+		copy(m[i*valueSize:(i+1)*valueSize], zeroes[:])
 		if i%trackFrequency == 0 {
 			fmt.Print(".")
 			m.Flush()
 		}
 	}
+	m.Flush()
 	fmt.Println()
 
 	w.Restart()
-	fmt.Printf("Zeroed map in %v, speed %v MB/s\n", w.elapsed, float64(capacity)/w.elapsed.Seconds()/1000000)
+	fmt.Printf("Zeroed map in %v, speed %v MB/s\n", w.elapsed, float64(capacity)/w.elapsed.Seconds()/(1024*1024))
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var values [1024]byte
 	for i := 0; i < transformCount; i++ {
-		j := r.Int63() % int64(capacity)
-		m[j]++
+		j := int(r.Int63() % int64(valueCount))
+		copy(values[:], m[j*valueSize:(j+1)*valueSize])
+		for k := 0; k < valueSize; k++ {
+			values[k]++
+		}
+		copy(m[j*valueSize:(j+1)*valueSize], values[:])
 		if i%trackFrequency == 0 {
 			fmt.Print(".")
 			m.Flush()
 		}
 	}
+	m.Flush()
 	fmt.Println()
 
 	w.Restart()
 	fmt.Printf("Incremented %v random bytes in %v, speed %v MB/s\n", transformCount, w.elapsed, float64(capacity)/w.elapsed.Seconds()/(1024*1024))
 
 	var sum int64
-	for i := 0; i < capacity; i++ {
-		sum += int64(m[i])
+	for i := 0; i < valueCount; i++ {
+		copy(values[:], m[i*valueSize:(i+1)*valueSize])
+		for k := 0; k < valueSize; k++ {
+			sum += int64(values[k])
+		}
 		if i%trackFrequency == 0 {
 			fmt.Print(".")
 		}
