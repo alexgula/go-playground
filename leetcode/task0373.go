08d0e4cd91ea89e8035a7d93de67d923e57a4f76
Oleksandr Gula <alexgula@gmail.com>
2017-08-25 16:17:55 +0200

    New still incorrect verson of a task

--- leetcode/task0373.go
+++ leetcode/task0373.go
@@ -1,5 +1,9 @@
 package task0373
 
+import (
+	"math"
+)
+
 func findKSmallestPairs(nums1 []int, nums2 []int, k int) [][]int {
 	result := make([][]int, k)
 
@@ -7,36 +11,33 @@ func findKSmallestPairs(nums1 []int, nums2 []int, k int) [][]int {
 
 	for i := 0; i < len(result); i++ {
 		result[i] = []int{nums1[i1], nums2[i2]}
-		if i1+1 == len(nums1) {
-			i1 = 0
-			i2++
-		} else if i2+1 == len(nums2) {
-			i1++
-			i2 = 0
-		} else if nums1[i1+1]+nums2[i2] < nums1[i1]+nums2[i2+1] {
-			i1++
-			i2 = 0
-		} else {
-			i1 = 0
-			i2++
-		}
+		i1, i2 = findSmallestPairAfter(nums1, nums2, i1, i2)
 	}
 
 	return result
 }
 
-type pairs struct {
-	nums1, nums2 []int
+func findSmallestPairAfter(nums1, nums2 []int, p1, p2 int) (int, int) {
+	var cur1, cur2 int
+	var cursum = math.MaxInt32
+	for i1 := 0; i1 < len(nums1); i1++ {
+		var i2 int
+		if i1 <= p1 {
+			if p2 >= len(nums2)-1 {
+				continue
+			}
+			i2 = p2 + 1
+		} else {
+			i2 = 0
+		}
+		var isum = pairSum(nums1, nums2, i1, i2)
+		if isum < cursum {
+			cur1, cur2, cursum = i1, i2, isum
+		}
+	}
+	return cur1, cur2
 }
 
-func (p pairs) getIth(i int) []int {
-	return []int{p.nums1[i%len(p.nums1)], p.nums2[i/len(p.nums1)]}
-}
-
-func (p pairs) less(i1 int, i2 int) {
-	return
-}
-
-func (p pairs) findKthSmallest(k, il, ir int) []int {
-	if l
+func pairSum(nums1, nums2 []int, p1, p2 int) int {
+	return nums1[p1] + nums2[p2]
 }
