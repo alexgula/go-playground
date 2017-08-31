915af3c3f30fd3256364328aa022f56a7da748de
Oleksandr Gula <alexgula@gmail.com>
2017-08-31 23:46:31 +0200

    Finally working task

--- leetcode/task0373.go
+++ leetcode/task0373.go
@@ -1,43 +1,68 @@
 package task0373
 
-import (
-	"math"
-)
+import "sort"
 
-func findKSmallestPairs(nums1 []int, nums2 []int, k int) [][]int {
+type bySum [][]int
+
+func (a bySum) Len() int           { return len(a) }
+func (a bySum) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a bySum) Less(i, j int) bool { return sum(a[i][0], a[i][1]) < sum(a[j][0], a[j][1]) }
+
+func kSmallestPairs(nums1 []int, nums2 []int, k int) [][]int {
+	if k > len(nums1)*len(nums2) {
+		k = len(nums1) * len(nums2)
+	}
+
 	result := make([][]int, k)
 
-	var i1, i2 int
+	var i, i1, i2 int
 
-	for i := 0; i < len(result); i++ {
+	for ; i < len(result); i++ {
 		result[i] = []int{nums1[i1], nums2[i2]}
-		i1, i2 = findSmallestPairAfter(nums1, nums2, i1, i2)
+		i1, i2 = nextPair(nums1, nums2, i1, i2)
 	}
+	sort.Sort(bySum(result))
+
+	for ; i < len(nums1)*len(nums2); i++ {
+		insertPair(nums1, nums2, result, i1, i2)
+		i1, i2 = nextPair(nums1, nums2, i1, i2)
+	}
+	sort.Sort(bySum(result))
 
 	return result
 }
 
-func findSmallestPairAfter(nums1, nums2 []int, p1, p2 int) (int, int) {
-	var cur1, cur2 int
-	var cursum = math.MaxInt32
-	for i1 := 0; i1 < len(nums1); i1++ {
-		var i2 int
-		if i1 <= p1 {
-			if p2 >= len(nums2)-1 {
-				continue
-			}
-			i2 = p2 + 1
-		} else {
-			i2 = 0
-		}
-		var isum = pairSum(nums1, nums2, i1, i2)
-		if isum < cursum {
-			cur1, cur2, cursum = i1, i2, isum
-		}
+func nextPair(nums1, nums2 []int, i1, i2 int) (int, int) {
+	i1++
+	if i1 >= len(nums1) {
+		i1 = 0
+		i2++
 	}
-	return cur1, cur2
+	return i1, i2
 }
 
-func pairSum(nums1, nums2 []int, p1, p2 int) int {
-	return nums1[p1] + nums2[p2]
+func insertPair(nums1, nums2 []int, pairs [][]int, i1, i2 int) {
+	var iMax, sumMax = findMax(pairs)
+	if sumPair(nums1, nums2, i1, i2) < sumMax {
+		pairs[iMax] = []int{nums1[i1], nums2[i2]}
+	}
+}
+
+func findMax(pairs [][]int) (int, int64) {
+	var iMax, sumMax = 0, sum(pairs[0][0], pairs[0][1])
+	for i := 1; i < len(pairs); i++ {
+		var sum = sum(pairs[i][0], pairs[i][1])
+		if sum > sumMax {
+			iMax, sumMax = i, sum
+		}
+	}
+	return iMax, sumMax
+}
+
+func sumPair(nums1, nums2 []int, i1, i2 int) int64 {
+	return sum(nums1[i1], nums2[i2])
+}
+
+func sum(val1, val2 int) int64 {
+	return int64(val1) + int64(val2)
 }
--- leetcode/task0373_test.go
+++ leetcode/task0373_test.go
@@ -1,6 +1,7 @@
 package task0373
 
 import (
+	"math"
 	"reflect"
 	"testing"
 )
@@ -14,11 +15,14 @@ func TestFindKSmallestPairs(t *testing.T) {
 	}{
 		{[]int{1, 7, 11}, []int{2, 4, 6}, 3, [][]int{{1, 2}, {1, 4}, {1, 6}}},
 		{[]int{1, 4, 11}, []int{2, 4, 6}, 3, [][]int{{1, 2}, {1, 4}, {4, 2}}},
-		{[]int{1, 4, 5}, []int{2, 4, 6}, 5, [][]int{{1, 2}, {1, 4}, {4, 2}, {1, 6}, {5, 2}}},
+		{[]int{1, 4, 5}, []int{2, 4, 6}, 5, [][]int{{1, 2}, {1, 4}, {4, 2}, {5, 2}, {1, 6}}},
+		{[]int{1, math.MaxInt32}, []int{1}, 5, [][]int{{1, 1}, {math.MaxInt32, 1}}},
+		{[]int{-4, 3}, []int{2, 5}, 4, [][]int{{-4, 2}, {-4, 5}, {3, 2}, {3, 5}}},
+		{[]int{1, 1, 2}, []int{1, 2, 3}, 10, [][]int{{1, 1}, {1, 1}, {2, 1}, {1, 2}, {1, 2}, {2, 2}, {1, 3}, {1, 3}, {2, 3}}},
 	}
 
 	for _, c := range cases {
-		got := findKSmallestPairs(c.nums1, c.nums2, c.k)
+		got := kSmallestPairs(c.nums1, c.nums2, c.k)
 		if !reflect.DeepEqual(got, c.expect) {
 			t.Errorf("nSmallestPairs(%v, %v, %v) == %v, expect %v", c.nums1, c.nums2, c.k, got, c.expect)
 		}
